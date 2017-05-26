2c0e1a2d495ee689f54281469ff7529f45f31631
jean-luc <jean-luc@pmpconcept.com>
2017-05-26 08:08:18 +0200

    add pentaminoes

---
+++ src/Pentamino.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Jlttt\Pentaminoes;
+
+
+class Pentamino
+{
+
+}
---
+++ tests/PentaminoFactoryTest.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Jlttt\Pentaminoes\PentaminoList;
+
+class PentaminoListTest
+{
+    private $list;
+
+    public function __construct()
+    {
+        $this->list
+    }
+}
